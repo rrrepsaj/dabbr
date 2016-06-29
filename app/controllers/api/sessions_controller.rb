953aca5a3b2af2effdb0d8938dedc23126d80d90
rrrepsaj <thejman222@gmail.com>
2016-06-29 10:32:59 -0700

    Begin building backend

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -3,13 +3,13 @@ class Api::SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_credentials(
+    @user = User.find_by_credentials(
       params[:user][:username],
       params[:user][:password]
     )
 
-    if user
-      sign_in(user)
+    if @user
+      sign_in(@user)
       render "api/users/show"
     else
       # flash.now[:errors] = ["Invalid username or password"]
@@ -18,12 +18,12 @@ class Api::SessionsController < ApplicationController
   end
 
   def destroy
-    user = current_user
-    if user
+    @user = current_user
+    if @user
       sign_out
       render "api/users/show"
     else
-      render json: { base: ["Nobody signed in"] }, status: 404
+      render json: { errors: ["Nobody signed in"] }, status: 404
     end
   end
 end
