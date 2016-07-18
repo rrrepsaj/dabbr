aa4c166483900a59ea39cbad9cfc4d645578902a
App Academy Student <student@appacademy.io>
2016-07-17 21:19:49 -0700

    Fix post time on index items

--- frontend/components/photo_index_item.jsx
+++ frontend/components/photo_index_item.jsx
@@ -3,6 +3,8 @@ const PhotoDetail = require('./photo_detail');
 const FadeModal = require('boron/FadeModal');
 const hashHistory = require('react-router').hashHistory;
 
+const Moment = require('moment');
+
 const PhotoIndexItem = React.createClass({
 	redirectToShow() {
 		const photoId = this.props.photo.id;
@@ -16,6 +18,32 @@ const PhotoIndexItem = React.createClass({
 
   render () {
 		let photo = this.props.photo;
+
+		// function timeSince(date) {
+	  //   var seconds = Math.floor((new Date() - date) / 1000);
+	  //   var interval = Math.floor(seconds / 31536000);
+	  //   if (interval > 1) {
+    //     return interval + " years";
+	  //   }
+	  //   interval = Math.floor(seconds / 2592000);
+	  //   if (interval > 1) {
+    //     return interval + " months";
+	  //   }
+	  //   interval = Math.floor(seconds / 86400);
+	  //   if (interval > 1) {
+    //     return interval + " days";
+	  //   }
+	  //   interval = Math.floor(seconds / 3600);
+	  //   if (interval > 1) {
+    //     return interval + " hours";
+	  //   }
+	  //   interval = Math.floor(seconds / 60);
+	  //   if (interval > 1) {
+    //     return interval + " minutes";
+	  //   }
+	  //   return Math.floor(seconds) + " seconds";
+		// }
+
 		return (
 			<div className="card clearfix">
 				<div className="photo-container">
@@ -29,8 +57,9 @@ const PhotoIndexItem = React.createClass({
 							</span>
 							<span className="photo-details">
 								<div className="name">
-									<span onClick={this.redirectToUserProfile}>{photo.user.username}</span>
+									<span onClick={this.redirectToUserProfile}>{photo.user.username} </span>
 									<span className="activity-item-date">
+										 · { Moment(photo.created_at).fromNow() }
 										{/*· {photo.created_at}*/}
 										{/*· 8 months ago*/}
 										<span className="recommended">{/* · Recommended */}</span>
